f62c84adec076eb30b729bb1f6c9551ca3c1557b
pesourob <maxines@seznam.cz>
2024-11-26 11:49:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Start the server
-/opt/ibm/wlp/bin/server start controller
+USERNAME=$USER
+USER_UID=$UID
+HOME_DIR=/home/$USER
+
+mkdir -p $HOME/.ssh
+chown -R $USER_UID:1001 $HOME_DIR/.ssh
+chmod -R 777 /home/$USERNAME
 
 # Spustit kolektivní příkaz
 collective create controller --keystorePassword=password123 --hostName=$HOSTNAME
+
+# Start the server
+/opt/ibm/wlp/bin/server start controller
 
 # Upravit soubor pomocí sed
 sed -i "s/<variable name=\"defaultHostName\" value=\"[^\"]*\" \/>/<variable name=\"defaultHostName\" value=\"$HOSTNAME\" \/>/" /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
