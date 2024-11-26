5133733416440a00a1042baa49c49ee6f46ccbf8
pesourob <maxines@seznam.cz>
2024-11-26 12:05:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-USERNAME=$USER
-USER_UID=$UID
-HOME_DIR=/home/$USER
-
-mkdir -p $HOME/.ssh
-chown -R $USER_UID:1001 $HOME_DIR/.ssh
-chmod -R 777 /home/$USERNAME
+# Upravit soubor pomocí sed
+sed -i "s/<variable name=\"defaultHostName\" value=\"[^\"]*\" \/>/<variable name=\"defaultHostName\" value=\"$HOSTNAME\" \/>/" /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
 
 # Spustit kolektivní příkaz
 collective create controller --keystorePassword=password123 --hostName=$HOSTNAME
 
 # Start the server
 /opt/ibm/wlp/bin/server start controller
-
-# Upravit soubor pomocí sed
-sed -i "s/<variable name=\"defaultHostName\" value=\"[^\"]*\" \/>/<variable name=\"defaultHostName\" value=\"$HOSTNAME\" \/>/" /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
 
 # Udržet kontejner běžící
 tail -f /logs/messages.log
