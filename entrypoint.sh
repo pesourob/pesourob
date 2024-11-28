bdfc1f0da6d54ca048d186affed9c286cb847a34
pesourob <maxines@seznam.cz>
2024-11-28 18:39:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,13 @@
 # Spustit kolektivní příkaz
 #collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=$HOSTNAME
 
-collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller-0.websphere-liberty-controller.pesourob-was-liberty.svc.cluster.local
+/opt/ibm/wlp/bin/server create controller
+
+/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller-0.websphere-liberty-controller.pesourob-was-liberty.svc.cluster.local
+
+chown -R 1001:0 /opt/ibm/wlp/usr
+chmod -R o+rwx /opt/ibm/wlp/usr/servers/controller/
+
 
 #sed -i 's/<quickStartSecurity userName="" userPassword="" />/<quickStartSecurity userName="admin" userPassword="password123"/>/' /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
 
