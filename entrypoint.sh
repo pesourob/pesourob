c7f903eb66dffbd2af5fb4424c9df5bad328d23c
pesourob <maxines@seznam.cz>
2024-11-29 00:26:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@
 
 /opt/ibm/wlp/bin/server create controller
 
-/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller-0.websphere-liberty-controller.pesourob-was-liberty.svc.cluster.local
+/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
 
 chown -R 1001:0 /opt/ibm/wlp/usr
 chmod -R o+rwx /opt/ibm/wlp/usr/servers/controller/
