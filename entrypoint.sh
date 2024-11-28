d6015dca8c04592024abb5db7ad9d0a1f191437f
pesourob <maxines@seznam.cz>
2024-11-28 13:31:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@
 # Spustit kolektivní příkaz
 collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=$HOSTNAME
 
+sed -i 's|<quickStartSecurity userName="" userPassword="" />|<quickStartSecurity userName="admin" userPassword="password123" />|' /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
+
 # Start the server
 /opt/ibm/wlp/bin/server start controller
 
