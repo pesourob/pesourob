5c640cf265f0c5ea8ce12f57055a4aac7ca804a6
pesourob <maxines@seznam.cz>
2024-11-28 16:35:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Upravit soubor pomocí sed
 #sed -i "s/<variable name=\"defaultHostName\" value=\"[^\"]*\" \/>/<variable name=\"defaultHostName\" value=\"$HOSTNAME\" \/>/" /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
 
-./server create controller && \
+#./server create controller && \
 
 # Spustit kolektivní příkaz
 collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=$HOSTNAME
