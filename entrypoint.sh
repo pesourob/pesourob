80446d00f42e8da576dc391aab0e8ba021a1b565
pesourob <maxines@seznam.cz>
2024-11-29 05:47:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-/opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log 
+#/opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log 
 
 # Spustit kolektivní příkaz
 
-#/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
+/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
 
+/opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log 
 #Pouzitelne v Docker
-#sed -i 's/<quickStartSecurity userName="" userPassword="" />/<quickStartSecurity userName="admin" userPassword="password123"/>/' /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml
+#sed -i 's/<quickStartSecurity userName="" userPassword="" />/<quickStartSecurity userName="admin" userPassword="password123"/>/' /opt/ibm/wlp/usr/servers/controller/collective-create-include.xm
