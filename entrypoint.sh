673feb989bc9113422f3a1dae2d2f8658dca7c13
pesourob <maxines@seznam.cz>
2024-11-29 08:47:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Creating directory /hello"
 mkdir -p /hello
 
 echo "Running collective command"
-/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/collective-create-include.xml --hostName=websphere-liberty-controller
+/opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
 
 echo "Stopping server"
 /opt/ibm/wlp/bin/server stop controller && tail -f /logs/messages.log
