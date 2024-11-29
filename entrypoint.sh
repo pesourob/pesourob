8002cdee03cb8eab95aa215ed579ee7bada672ae
pesourob <maxines@seznam.cz>
2024-11-29 07:20:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 /opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log 
 
+mkdir -p /hello
+
 # Spustit kolektivní příkaz
 
 /opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
