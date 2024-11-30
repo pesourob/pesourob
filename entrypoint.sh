953f201c4e3087c0b36839eac88e51b392c49759
pesourob <maxines@seznam.cz>
2024-11-30 11:23:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo "Starting server"
+echo "----------Starting server----------"
 /opt/ibm/wlp/bin/server start controller 
 
 if [[ ! -e /opt/ibm/wlp/usr/servers/controller/resources || ! -e /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml ]]; then
-    echo "Running collective command"
+    echo "----------Running collective command----------"
     /opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
 else
-    echo "Files exist, skipping collective create command"
+    echo "----------Files exist, skipping collective create command----------"
 fi
 
-echo "Stopping server"
+echo "----------Stopping server----------"
 /opt/ibm/wlp/bin/server stop controller 
 
-echo "Starting server"
+#Symbolic link
+echo "----------Creating symbolic link----------"
+ln -s /opt/ibm/wlp/usr/servers/controller/ config
+
+echo "----------Starting server----------"
 /opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log
