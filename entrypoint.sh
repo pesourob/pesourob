a81731f52423af42cb3147cea8ee9d4364b4755f
pesourob <maxines@seznam.cz>
2024-11-30 17:38:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 echo "----------Starting server----------"
-/opt/ibm/wlp/bin/server start controller 
+/config/ibm/wlp/bin/server start controller 
 
-if [[ ! -e /opt/ibm/wlp/usr/servers/controller/resources || ! -e /opt/ibm/wlp/usr/servers/controller/collective-create-include.xml ]]; then
+if [[ ! -e /config/ibm/wlp/usr/servers/controller/resources || ! -e /config/ibm/wlp/usr/servers/controller/collective-create-include.xml ]]; then
     echo "----------Running collective command----------"
-    /opt/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/opt/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
+    /config/ibm/wlp/bin/collective create controller --keystorePassword=password123 --createConfigFile=/config/ibm/wlp/usr/servers/controller/ --hostName=websphere-liberty-controller
 else
     echo "----------Files exist, skipping collective create command----------"
 fi
 
 echo "----------Stopping server----------"
-/opt/ibm/wlp/bin/server stop controller 
+/config/ibm/wlp/bin/server stop controller 
 
 mkdir -p /controller
 
 #Symbolic link
 echo "----------Creating symbolic link----------"
-ln -s /opt/ibm/wlp/usr/servers/controller /controller
+#ln -s /config/ibm/wlp/usr/servers/controller /controller
 
 echo "----------Starting server----------"
-/opt/ibm/wlp/bin/server start controller && tail -f /logs/messages.log
+/config/ibm/wlp/bin/server start controller && tail -f /logs/messages.log
